395c26fd550914e05c02607ea3707e2c326ae4cb
Alex Zhdanov <doctor@Alexs-MacBook-Air.local>
2025-09-02 23:28:52 +0300

    fix

--- internal/handler/endpoints.go
+++ internal/handler/endpoints.go
@@ -4,7 +4,7 @@ import (
 	"encoding/json"
 	"io"
 	"net/http"
-	"strconv"
+	//"strconv"
 
 	"github.com/Doctor46-create/urlshort/internal/model"
 	"github.com/go-chi/chi/v5"
@@ -107,7 +107,7 @@ func (h *urlHandler) ShortenURLJSON(w http.ResponseWriter, r *http.Request) {
 	}
 
 	w.Header().Set("Content-Type", "application/json")
-	w.Header().Set("Content-Length", strconv.Itoa(len(jsonData)))
+	//w.Header().Set("Content-Length", strconv.Itoa(len(jsonData)))
 	w.WriteHeader(http.StatusCreated)
 	w.Write(jsonData)
 
