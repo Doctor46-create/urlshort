e77db093cfd98a89b47bfa3bdd342aa29c426a68
Alex Zhdanov <doctor@Alexs-MacBook-Air.local>
2025-12-14 17:09:08 +0300

    fix

--- internal/handler/router.go
+++ internal/handler/router.go
@@ -1,6 +1,9 @@
 package handler
 
 import (
+	"net/http"
+	"net/http/pprof"
+
 	"github.com/Doctor46-create/urlshort/internal/audit"
 	"github.com/Doctor46-create/urlshort/internal/config"
 	"github.com/Doctor46-create/urlshort/internal/config/db"
@@ -39,5 +42,46 @@ func (h *Handler) InitRouter(logger *zap.Logger) chi.Router {
 	r.With(GetOnly(logger)).Get("/ping", h.urlHandler.PingDB)
 	r.With(GetOnly(logger)).Get("/api/user/urls", h.urlHandler.GetUserURLs)
 	r.Delete("/api/user/urls", h.urlHandler.DeleteURLs)
+
+	if h.enablePProf() {
+		r.Mount("/debug", h.pprofRouter())
+		h.logger.Info("pprof profiling enabled at /debug/pprof/")
+	}
 	return r
+}
+
+func (h *Handler) pprofRouter() chi.Router {
+	r := chi.NewRouter()
+	
+	r.Use(h.pprofAuthMiddleware)
+	
+	r.Get("/pprof", pprof.Index)
+	
+	r.Get("/pprof/cmdline", pprof.Cmdline)
+	r.Get("/pprof/profile", pprof.Profile)
+	r.Get("/pprof/symbol", pprof.Symbol)
+	r.Get("/pprof/trace", pprof.Trace)
+	
+	r.Get("/pprof/goroutine", pprof.Handler("goroutine").ServeHTTP)
+	r.Get("/pprof/heap", pprof.Handler("heap").ServeHTTP)
+	r.Get("/pprof/threadcreate", pprof.Handler("threadcreate").ServeHTTP)
+	r.Get("/pprof/block", pprof.Handler("block").ServeHTTP)
+	r.Get("/pprof/mutex", pprof.Handler("mutex").ServeHTTP)
+	r.Get("/pprof/allocs", pprof.Handler("allocs").ServeHTTP)
+	
+	return r
+}
+
+func (h *Handler) enablePProf() bool {
+	return true
+}
+
+func (h *Handler) pprofAuthMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (h *Handler) GetPProfRouter() chi.Router {
+	return h.pprofRouter()
 }
