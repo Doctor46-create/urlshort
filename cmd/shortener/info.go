692abe1a85f3f58a478752a9e44b681bd7b8bb70
Alex Zhdanov <doctor@Alexs-MacBook-Air.local>
2025-12-18 15:27:08 +0300

    iteration 23

---
+++ cmd/shortener/info.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+var buildVersion string
+var buildDate string
+var buildCommit string
+
+func showBuildInfo() {
+	version := buildVersion
+	if version == "" {
+		version = "N/A"
+	}
+
+	date := buildDate
+	if date == "" {
+		date = "N/A"
+	}
+
+	commit := buildCommit
+	if commit == "" {
+		commit = "N/A"
+	}
+
+	fmt.Printf("Build version: %s\n", version)
+	fmt.Printf("Build date: %s\n", date)
+	fmt.Printf("Build commit: %s\n", commit)
+}
+
